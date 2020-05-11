081e3a85c9d4ade2c6c81fb11d1c54266b563848
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-05-11 14:06:57 +0800

    Create harbour-fernweh-zh_CN.ts

---
+++ translations/harbour-fernweh-zh_CN.ts
@@ -0,0 +1,872 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<!DOCTYPE TS>
+
+<TS version="2.1">
+
+<context>
+
+    <name>AboutPage</name>
+
+    <message>
+
+        <source>About Fernweh</source>
+
+        <translation>关于 Fernweh</translation>
+
+    </message>
+
+    <message>
+
+        <source>A Flickr client for Sailfish OS</source>
+
+        <translation>一个旗鱼系统 Flickr 客户端</translation>
+
+    </message>
+
+    <message>
+
+        <source>By Sebastian J. Wolf</source>
+
+        <translation>作者为 Sebastian J. Wolf</translation>
+
+    </message>
+
+    <message>
+
+        <source>Send E-Mail</source>
+
+        <translation>发送电子邮件</translation>
+
+    </message>
+
+    <message>
+
+        <source>Licensed under GNU GPLv3</source>
+
+        <translation>采用 GNU GPLv3 许可协议</translation>
+
+    </message>
+
+    <message>
+
+        <source>Sources on GitHub</source>
+
+        <translation>源代码位于 Github</translation>
+
+    </message>
+
+    <message>
+
+        <source>About Flickr</source>
+
+        <translation>关于 Flickr</translation>
+
+    </message>
+
+    <message>
+
+        <source>Terms of Service</source>
+
+        <translation>服务条款</translation>
+
+    </message>
+
+    <message>
+
+        <source>Privacy Policy</source>
+
+        <translation>隐私政策</translation>
+
+    </message>
+
+    <message>
+
+        <source>Credits</source>
+
+        <translation>信誉</translation>
+
+    </message>
+
+    <message>
+
+        <source>This project uses OAuth for Qt by Akos Polster. Thanks for making it available under the conditions of the BSD-2-Clause license!</source>
+
+        <translation>该项目采用 Akos Polster 开发的 OAuth for Qt 。感谢使它在  BSD-2-Clause 许可协议下可用。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Open OAuth for Qt on GitHub</source>
+
+        <translation>在 Github 打开 OAuth for Qt 。</translation>
+
+    </message>
+
+    <message>
+
+        <source>This product uses the Flickr API but is not endorsed or certified by SmugMug, Inc.</source>
+
+        <translation>该产品使用了 Flickr API 但并未得到 SmugMug, Inc. 背书或认证。</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>Album</name>
+
+    <message>
+
+        <source>Copy URL to Clipboard</source>
+
+        <translation>复制链接到剪切板</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1 photos</source>
+
+        <translation>%1 张图片</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>BackgroundProgressIndicator</name>
+
+    <message>
+
+        <source>%1 %</source>
+
+        <translation>%1 %</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1</source>
+
+        <translation>%1<</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>ImagePage</name>
+
+    <message>
+
+        <source>Download of %1 successful.</source>
+
+        <translation>已成功下载 %1 。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Download failed.</source>
+
+        <translation>下载失败。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Photo Details</source>
+
+        <translation>图片详情</translation>
+
+    </message>
+
+    <message>
+
+        <source>Title</source>
+
+        <translation>标题</translation>
+
+    </message>
+
+    <message>
+
+        <source>n/a</source>
+
+        <translation>n/a</translation>
+
+    </message>
+
+    <message>
+
+        <source>Date Taken</source>
+
+        <translation>拍摄日期</translation>
+
+    </message>
+
+    <message>
+
+        <source>Views</source>
+
+        <translation>评论</translation>
+
+    </message>
+
+    <message>
+
+        <source>Author</source>
+
+        <translation>作者</translation>
+
+    </message>
+
+    <message>
+
+        <source>License</source>
+
+        <translation>许可协议</translation>
+
+    </message>
+
+    <message>
+
+        <source>Location</source>
+
+        <translation>位置</translation>
+
+    </message>
+
+    <message>
+
+        <source>Copy URL to Clipboard</source>
+
+        <translation>复制链接到剪切板</translation>
+
+    </message>
+
+    <message>
+
+        <source>Open in Browser</source>
+
+        <translation>在浏览器打开</translation>
+
+    </message>
+
+    <message>
+
+        <source>Download Photo</source>
+
+        <translation>下载图片</translation>
+
+    </message>
+
+    <message>
+
+        <source>Go to Author&apos;s Profile</source>
+
+        <translation>前往作者简介</translation>
+
+    </message>
+
+    <message>
+
+        <source>Favorited</source>
+
+        <translation>已收藏</translation>
+
+    </message>
+
+    <message>
+
+        <source>Camera</source>
+
+        <translation>相机</translation>
+
+    </message>
+
+    <message>
+
+        <source>Description</source>
+
+        <translation>描述</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>ImageProgressIndicator</name>
+
+    <message>
+
+        <source>%1 %</source>
+
+        <translation>%1 %</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1</source>
+
+        <translation>%1</translation>
+
+    </message>
+
+    <message>
+
+        <source>Error</source>
+
+        <translation>错误</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>LoadingIndicator</name>
+
+    <message>
+
+        <source>Loading...</source>
+
+        <translation>加载中……</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>OverviewPage</name>
+
+    <message>
+
+        <source>About Fernweh</source>
+
+        <translation>关于 Fernweh</translation>
+
+    </message>
+
+    <message>
+
+        <source>Saying hello to Flickr...</source>
+
+        <translation>正在访问 Flickr……</translation>
+
+    </message>
+
+    <message>
+
+        <source>Fernweh could not log you in!</source>
+
+        <translation>Fernweh 无法登录到你的账户！</translation>
+
+    </message>
+
+    <message>
+
+        <source>Try again</source>
+
+        <translation>重试</translation>
+
+    </message>
+
+    <message>
+
+        <source>Authenticate</source>
+
+        <translation>授权</translation>
+
+    </message>
+
+    <message>
+
+        <source>Loading own pictures...</source>
+
+        <translation>加载我的照片……</translation>
+
+    </message>
+
+    <message>
+
+        <source>Search on Flickr...</source>
+
+        <translation>在 Flickr 搜索</translation>
+
+    </message>
+
+    <message>
+
+        <source>Searching...</source>
+
+        <translation>正在搜索……</translation>
+
+    </message>
+
+    <message>
+
+        <source>What are you looking for?</source>
+
+        <translation>你想找什么?</translation>
+
+    </message>
+
+    <message>
+
+        <source>No results found</source>
+
+        <translation>没有找到搜索结果</translation>
+
+    </message>
+
+    <message>
+
+        <source>Settings</source>
+
+        <translation>设置</translation>
+
+    </message>
+
+    <message>
+
+        <source>Loading profile...</source>
+
+        <translation>正在加载简介……</translation>
+
+    </message>
+
+    <message>
+
+        <source>No photos found. Upload more photos to see more here! ;)</source>
+
+        <translation>没有找到图片。请在此上传更多图片:)</translation>
+
+    </message>
+
+    <message>
+
+        <source>No albums found. Upload more albums to see more here! ;)</source>
+
+        <translation>没有找到相册。请在此上传更多相册:)</translation>
+
+    </message>
+
+    <message>
+
+        <source>Load more photos</source>
+
+        <translation>加载更多图片</translation>
+
+    </message>
+
+    <message>
+
+        <source>Load more albums</source>
+
+        <translation>加载更多相册</translation>
+
+    </message>
+
+    <message>
+
+        <source>No more interesting photos found. Well, that&apos;s sad... :(</source>
+
+        <translation>非常抱歉 :(暂未找到更多有趣的图片。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Load more pictures</source>
+
+        <translation>加载更多图片</translation>
+
+    </message>
+
+    <message>
+
+        <source>Load more results</source>
+
+        <translation>加载更多结果</translation>
+
+    </message>
+
+    <message>
+
+        <source>Refresh</source>
+
+        <translation>刷新</translation>
+
+    </message>
+
+    <message>
+
+        <source>No more photos found! That seems to be all for your search... :(</source>
+
+        <translation>没有找到更多图片！似乎这就是你搜索结果的全部内容:(</translation>
+
+    </message>
+
+    <message>
+
+        <source>No own photos found</source>
+
+        <translation>没有找到自己的图片</translation>
+
+    </message>
+
+    <message>
+
+        <source>No own albums found</source>
+
+        <translation>没有找到自己的相册</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>OwnAlbumsButton</name>
+
+    <message>
+
+        <source>Albums</source>
+
+        <translation>相册</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>OwnPicturesButton</name>
+
+    <message>
+
+        <source>Photos</source>
+
+        <translation>图片</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>Profile</name>
+
+    <message>
+
+        <source>Joined in %1</source>
+
+        <translation>已加入 %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1 Photos</source>
+
+        <translation> %1 张图片</translation>
+
+    </message>
+
+    <message>
+
+        <source>Statistics</source>
+
+        <translation>数据</translation>
+
+    </message>
+
+    <message>
+
+        <source>&lt;b&gt;Total Views:&lt;/b&gt; %1</source>
+
+        <translation>&lt;b&gt;所有评论:&lt;/b&gt; %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>&lt;b&gt;Photo Views:&lt;/b&gt; %1</source>
+
+        <translation>&lt;b&gt;图片评论:&lt;/b&gt; %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>&lt;b&gt;Album Views:&lt;/b&gt; %1</source>
+
+        <translation>&lt;b&gt;相册评论:&lt;/b&gt; %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>&lt;b&gt;Photostream Views:&lt;/b&gt; %1</source>
+
+        <translation>&lt;b&gt;图片流评论:&lt;/b&gt; %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>&lt;b&gt;Collection Views:&lt;/b&gt; %1</source>
+
+        <translation>&lt;b&gt;收藏评论:&lt;/b&gt; %</translation>
+
+    </message>
+
+    <message>
+
+        <source>&lt;b&gt;Gallery Views:&lt;/b&gt; %1</source>
+
+        <translation>&lt;b&gt;相册评论:&lt;/b&gt; %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>No statistics available</source>
+
+        <translation>无更多可用数据</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>ProfileButton</name>
+
+    <message>
+
+        <source>Profile</source>
+
+        <translation>简介</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>ProfilePage</name>
+
+    <message>
+
+        <source>Open in Browser</source>
+
+        <translation>在浏览器打开</translation>
+
+    </message>
+
+    <message>
+
+        <source>Copy URL to Clipboard</source>
+
+        <translation>复制链接到剪切板</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>SearchButton</name>
+
+    <message>
+
+        <source>Search</source>
+
+        <translation>搜索</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>SettingsPage</name>
+
+    <message>
+
+        <source>Settings</source>
+
+        <translation>设置</translation>
+
+    </message>
+
+    <message>
+
+        <source>Style</source>
+
+        <translation>风格</translation>
+
+    </message>
+
+    <message>
+
+        <source>Enable swipe navigation</source>
+
+        <translation>启用滑动导航</translation>
+
+    </message>
+
+    <message>
+
+        <source>Use swipe navigation to switch categories (e.g. from pictures to albums)</source>
+
+        <translation>使用滑动导航以切换分类（例如，从图片切换到相册）</translation>
+
+    </message>
+
+    <message>
+
+        <source>Font Size</source>
+
+        <translation>字体大小</translation>
+
+    </message>
+
+    <message>
+
+        <source>Choose the font size here</source>
+
+        <translation>在此选择字体大小</translation>
+
+    </message>
+
+    <message>
+
+        <source>Normal (Fernweh default)</source>
+
+        <translation>正常（Fernweh 默认）</translation>
+
+    </message>
+
+    <message>
+
+        <source>Large (Sailfish OS default)</source>
+
+        <translation>大（旗鱼系统默认）</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>TrendingButton</name>
+
+    <message>
+
+        <source>Trending</source>
+
+        <translation>趋势</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>WelcomePage</name>
+
+    <message>
+
+        <source>OK</source>
+
+        <translation>确认</translation>
+
+    </message>
+
+    <message>
+
+        <source>Please enter the Flickr PIN:</source>
+
+        <translation>请输入Flickr PIN:</translation>
+
+    </message>
+
+    <message>
+
+        <source>Unable to authenticate you with the entered PIN.</source>
+
+        <translation>无法用输入的 PIN 进行身份验证。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Enter PIN again</source>
+
+        <translation>请再次输入 PIN</translation>
+
+    </message>
+
+    <message>
+
+        <source>Restart authentication</source>
+
+        <translation>重启身份验证</translation>
+
+    </message>
+
+    <message>
+
+        <source>Welcome to Fernweh!</source>
+
+        <translation>欢迎使用 Fernweh!</translation>
+
+    </message>
+
+    <message>
+
+        <source>Please login to Flickr to continue.</source>
+
+        <translation>请登录 Flickr 以继续。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Log in to Flickr</source>
+
+        <translation>登录到 Flickr</translation>
+
+    </message>
+
+    <message>
+
+        <source>If you don&apos;t have a Flickr account yet, please sign up first.</source>
+
+        <translation>如果你尚无 Flickr 账户，请先注册。</translation>
+
+    </message>
+
+    <message>
+
+        <source>Sign up for Flickr</source>
+
+        <translation>已登录 Flickr </translation>
+
+    </message>
+
+</context>
+
+</TS>
+
