a6148d69e812d91bedc42ac1bfbb46cbd8cb652a
legacychimera247 <mad_fer_it@msn.com>
2023-09-04 02:41:26 +0200

    Create harbour-fernweh_it.ts
    Started the Italian translation, would love to see the app finally translated into my language...

---
+++ translations/harbour-fernweh_it.ts
@@ -0,0 +1,436 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Fernweh</source>
+        <translation type=A proposito di Fernweh></translation>
+    </message>
+    <message>
+        <source>A Flickr client for Sailfish OS</source>
+        <translation type=Un client Flickr per Sailfish OS></translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation type=Creato da Sebastian J. Wolf></translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation type=Invia E-Mail></translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation type=Su licenza GNU GPLv3></translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation type=Sorgenti su Github></translation>
+    </message>
+    <message>
+        <source>About Flickr</source>
+        <translation type=A proposito di Flickr></translation>
+    </message>
+    <message>
+        <source>Terms of Service</source>
+        <translation type=Termini di Servizio></translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation type=Policy sulla riservatezza></translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type=Crediti></translation>
+    </message>
+    <message>
+        <source>This project uses OAuth for Qt by Akos Polster. Thanks for making it available under the conditions of the BSD-2-Clause license!</source>
+        <translation type=Questo progetto usa la libreria OAuth per Qt di Akos Polster. Grazie per averla resa disponibile sotto licenza BSD-2-Clause></translation>
+    </message>
+    <message>
+        <source>Open OAuth for Qt on GitHub</source>
+        <translation type=OAuth per Qt su Github></translation>
+    </message>
+    <message>
+        <source>This product uses the Flickr API but is not endorsed or certified by SmugMug, Inc.</source>
+        <translation type=Questo prodotto usa l'API di Flickr ma non è sponsorizzato né certificato da SmugMug, Inc.></translation>
+    </message>
+</context>
+<context>
+    <name>Album</name>
+    <message>
+        <source>Copy URL to Clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 photos</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundProgressIndicator</name>
+    <message>
+        <source>%1 %</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImagePage</name>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Photo Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>n/a</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date Taken</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Views</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy URL to Clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download Photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Go to Author&apos;s Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Favorited</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageProgressIndicator</name>
+    <message>
+        <source>%1 %</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About Fernweh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saying hello to Flickr...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fernweh could not log you in!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Authenticate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loading own pictures...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search on Flickr...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Searching...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>What are you looking for?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loading profile...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No photos found. Upload more photos to see more here! ;)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No albums found. Upload more albums to see more here! ;)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more photos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more albums</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No more interesting photos found. Well, that&apos;s sad... :(</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more pictures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load more results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No more photos found! That seems to be all for your search... :(</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No own photos found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No own albums found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OwnAlbumsButton</name>
+    <message>
+        <source>Albums</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OwnPicturesButton</name>
+    <message>
+        <source>Photos</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Joined in %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 Photos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Total Views:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Photo Views:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Album Views:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Photostream Views:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Collection Views:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Gallery Views:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No statistics available</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProfileButton</name>
+    <message>
+        <source>Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePage</name>
+    <message>
+        <source>Open in Browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy URL to Clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchButton</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable swipe navigation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use swipe navigation to switch categories (e.g. from pictures to albums)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose the font size here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Normal (Fernweh default)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Large (Sailfish OS default)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrendingButton</name>
+    <message>
+        <source>Trending</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter the Flickr PIN:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to authenticate you with the entered PIN.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter PIN again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Restart authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Welcome to Fernweh!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please login to Flickr to continue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log in to Flickr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you don&apos;t have a Flickr account yet, please sign up first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sign up for Flickr</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
