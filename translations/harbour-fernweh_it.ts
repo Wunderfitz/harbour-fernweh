7033ff2970a3091c277e7abf408d6f1df945bb3e
legacychimera247 <mad_fer_it@msn.com>
2023-09-04 14:34:10 +0200

    Update harbour-fernweh_it.ts

--- translations/harbour-fernweh_it.ts
+++ translations/harbour-fernweh_it.ts
@@ -37,7 +37,7 @@
     </message>
     <message>
         <source>Privacy Policy</source>
-        <translation type=Policy sulla riservatezza></translation>
+        <translation type=Accordo sulla riservatezza></translation>
     </message>
     <message>
         <source>Credits</source>
@@ -60,377 +60,376 @@
     <name>Album</name>
     <message>
         <source>Copy URL to Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation type=Copia URL negli Appunti></translation>
     </message>
     <message>
         <source>%1 photos</source>
-        <translation type="unfinished"></translation>
+        <translation type=%1 foto></translation>
     </message>
 </context>
 <context>
     <name>BackgroundProgressIndicator</name>
     <message>
         <source>%1 %</source>
-        <translation type="unfinished"></translation>
+        <translation type=%1 %></translation>
     </message>
     <message>
         <source>%1</source>
-        <translation type="unfinished"></translation>
+        <translation type=%1></translation>
     </message>
 </context>
 <context>
     <name>ImagePage</name>
     <message>
         <source>Download of %1 successful.</source>
-        <translation type="unfinished"></translation>
+        <translation type=Scaricamento di %1 completato></translation>
     </message>
     <message>
         <source>Download failed.</source>
-        <translation type="unfinished"></translation>
+        <translation type=Scaricamento fallito></translation>
     </message>
     <message>
         <source>Photo Details</source>
-        <translation type="unfinished"></translation>
+        <translation type=Dettagli foto></translation>
     </message>
     <message>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation type=Titolo></translation>
     </message>
     <message>
         <source>n/a</source>
-        <translation type="unfinished"></translation>
+        <translation type=n/a></translation>
     </message>
     <message>
         <source>Date Taken</source>
-        <translation type="unfinished"></translation>
+        <translation type=Data scatto></translation>
     </message>
     <message>
         <source>Views</source>
-        <translation type="unfinished"></translation>
+        <translation type=Visualizzazioni></translation>
     </message>
     <message>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation type=Autore></translation>
     </message>
     <message>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation type=Licenza></translation>
     </message>
     <message>
         <source>Location</source>
-        <translation type="unfinished"></translation>
+        <translation type=Luogo></translation>
     </message>
     <message>
         <source>Copy URL to Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation type=Copia URL negli Appunti></translation>
     </message>
     <message>
         <source>Open in Browser</source>
-        <translation type="unfinished"></translation>
+        <translation type=Apri nel Browser></translation>
     </message>
     <message>
         <source>Download Photo</source>
-        <translation type="unfinished"></translation>
+        <translation type=Scarica Foto></translation>
     </message>
     <message>
         <source>Go to Author&apos;s Profile</source>
-        <translation type="unfinished"></translation>
+        <translation type=Vai sul profilo dell'autore></translation>
     </message>
     <message>
         <source>Favorited</source>
-        <translation type="unfinished"></translation>
+        <translation type=Preferite></translation>
     </message>
     <message>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation type=Fotocamera></translation>
     </message>
     <message>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation type=Descrizione></translation>
     </message>
 </context>
 <context>
     <name>ImageProgressIndicator</name>
     <message>
         <source>%1 %</source>
-        <translation type="unfinished"></translation>
+        <translation type=%1 %></translation>
     </message>
     <message>
         <source>%1</source>
-        <translation type="unfinished"></translation>
+        <translation type=%1></translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type=Errore></translation>
     </message>
 </context>
 <context>
     <name>LoadingIndicator</name>
     <message>
         <source>Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation type=Sto caricando...></translation>
     </message>
 </context>
 <context>
     <name>OverviewPage</name>
     <message>
         <source>About Fernweh</source>
-        <translation type="unfinished"></translation>
+        <translation type=A proposito di Fernweh></translation>
     </message>
     <message>
         <source>Saying hello to Flickr...</source>
-        <translation type="unfinished"></translation>
+        <translation type=Dì ciao a Flickr></translation>
     </message>
     <message>
         <source>Fernweh could not log you in!</source>
-        <translation type="unfinished"></translation>
+        <translation type=Fernweh non è riuscito ad entrare></translation>
     </message>
     <message>
         <source>Try again</source>
-        <translation type="unfinished"></translation>
+        <translation type=Prova di nuovo></translation>
     </message>
     <message>
         <source>Authenticate</source>
-        <translation type="unfinished"></translation>
+        <translation type=Autenticati></translation>
     </message>
     <message>
         <source>Loading own pictures...</source>
-        <translation type="unfinished"></translation>
+        <translation type=Sto caricando le tue immagini...></translation>
     </message>
     <message>
         <source>Search on Flickr...</source>
-        <translation type="unfinished"></translation>
+        <translation type=Cerca su Flickr></translation>
     </message>
     <message>
         <source>Searching...</source>
-        <translation type="unfinished"></translation>
+        <translation type=Sto cercando...></translation>
     </message>
     <message>
         <source>What are you looking for?</source>
-        <translation type="unfinished"></translation>
+        <translation type=Che stai cercando?></translation>
     </message>
     <message>
         <source>No results found</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessun risultato></translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type=Impostazioni></translation>
     </message>
     <message>
         <source>Loading profile...</source>
-        <translation type="unfinished"></translation>
+        <translation type=Sto caricando il profilo></translation>
     </message>
     <message>
         <source>No photos found. Upload more photos to see more here! ;)</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessuna foto trovata. Carica qualche foto per vedere qualcosa qui! ;)></translation>
     </message>
     <message>
         <source>No albums found. Upload more albums to see more here! ;)</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessun album trovato. Carica qualche album per vedere qualcosa qui! ;)></translation>
     </message>
     <message>
         <source>Load more photos</source>
-        <translation type="unfinished"></translation>
+        <translation type=Carica altre foto></translation>
     </message>
     <message>
         <source>Load more albums</source>
-        <translation type="unfinished"></translation>
+        <translation type=Carica altri album></translation>
     </message>
     <message>
         <source>No more interesting photos found. Well, that&apos;s sad... :(</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessun altra foto interessante trovata. È triste, sai? :(></translation>
     </message>
     <message>
         <source>Load more pictures</source>
-        <translation type="unfinished"></translation>
+        <translation type=Carica altre foto></translation>
     </message>
     <message>
         <source>Load more results</source>
-        <translation type="unfinished"></translation>
+        <translation type=Carica altri risultati></translation>
     </message>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation type=Ricarica></translation>
     </message>
     <message>
         <source>No more photos found! That seems to be all for your search... :(</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessun altra foto trovata! Uh, sembra la fine delle tue ricerche...></translation>
     </message>
     <message>
         <source>No own photos found</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessuna tua foto trovata></translation>
     </message>
     <message>
         <source>No own albums found</source>
-        <translation type="unfinished"></translation>
+        <translation type=Nessun tuo album trovato></translation>
     </message>
 </context>
 <context>
     <name>OwnAlbumsButton</name>
     <message>
         <source>Albums</source>
-        <translation type="unfinished"></translation>
+        <translation type=Album></translation>
     </message>
 </context>
 <context>
     <name>OwnPicturesButton</name>
     <message>
         <source>Photos</source>
-        <translation type="unfinished"></translation>
+        <translation type=Foto></translation>
     </message>
 </context>
 <context>
     <name>Profile</name>
     <message>
         <source>Joined in %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=Entrato in %1></translation>
     </message>
     <message>
         <source>%1 Photos</source>
-        <translation type="unfinished"></translation>
+        <translation type=%1 foto></translation>
     </message>
     <message>
         <source>Statistics</source>
-        <translation type="unfinished"></translation>
+        <translation type=Statistiche></translation>
     </message>
     <message>
         <source>&lt;b&gt;Total Views:&lt;/b&gt; %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=&lt;b&gt;Visualizzazioni totali;/b&gt; %1></translation>
     </message>
     <message>
         <source>&lt;b&gt;Photo Views:&lt;/b&gt; %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=&lt;b&gt;Visualizzazioni foto;/b&gt; %1></translation>
     </message>
     <message>
         <source>&lt;b&gt;Album Views:&lt;/b&gt; %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=&lt;b&gt;Visualizzazioni album;/b&gt; %1></translation>
     </message>
     <message>
         <source>&lt;b&gt;Photostream Views:&lt;/b&gt; %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=&lt;b&gt;Visualizzazioni flusso di foto;/b&gt; %1></translation>
     </message>
     <message>
         <source>&lt;b&gt;Collection Views:&lt;/b&gt; %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=&lt;b&gt;Visualizzazioni collezioni;/b&gt; %1></translation>
     </message>
     <message>
         <source>&lt;b&gt;Gallery Views:&lt;/b&gt; %1</source>
-        <translation type="unfinished"></translation>
+        <translation type=&lt;b&gt;Visualizzazioni galleria;/b&gt; %1></translation>
     </message>
     <message>
         <source>No statistics available</source>
-        <translation type="unfinished"></translation>
+        <translation type=Statistiche non disponibili></translation>
     </message>
 </context>
 <context>
     <name>ProfileButton</name>
     <message>
-        <source>Profile</source>
-        <translation type="unfinished"></translation>
+        <source>Profile</source>Profilo"unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ProfilePage</name>
     <message>
         <source>Open in Browser</source>
-        <translation type="unfinished"></translation>
+        <translation type=Apri nel Browser></translation>
     </message>
     <message>
         <source>Copy URL to Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation type=Copia URL negli Appunti></translation>
     </message>
 </context>
 <context>
     <name>SearchButton</name>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation type=Cerca></translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type=Impostazioni></translation>
     </message>
     <message>
         <source>Style</source>
-        <translation type="unfinished"></translation>
+        <translation type=Stile></translation>
     </message>
     <message>
         <source>Enable swipe navigation</source>
-        <translation type="unfinished"></translation>
+        <translation type=Abilita navigazione con gesti></translation>
     </message>
     <message>
         <source>Use swipe navigation to switch categories (e.g. from pictures to albums)</source>
-        <translation type="unfinished"></translation>
+        <translation type=Usa la navigazione a gesti per cambiare categoria (es. da foto ad album)></translation>
     </message>
     <message>
         <source>Font Size</source>
-        <translation type="unfinished"></translation>
+        <translation type=Dimensione carattere></translation>
     </message>
     <message>
         <source>Choose the font size here</source>
-        <translation type="unfinished"></translation>
+        <translation type=Scegli dimensione carattere></translation>
     </message>
     <message>
         <source>Normal (Fernweh default)</source>
-        <translation type="unfinished"></translation>
+        <translation type=Normale (predefinito di Fernweh)></translation>
     </message>
     <message>
         <source>Large (Sailfish OS default)</source>
-        <translation type="unfinished"></translation>
+        <translation type=Largo (predefinito di Sailfish OS)></translation>
     </message>
 </context>
 <context>
     <name>TrendingButton</name>
     <message>
         <source>Trending</source>
-        <translation type="unfinished"></translation>
+        <translation type=Di tendenza></translation>
     </message>
 </context>
 <context>
     <name>WelcomePage</name>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type=Va bene></translation>
     </message>
     <message>
         <source>Please enter the Flickr PIN:</source>
-        <translation type="unfinished"></translation>
+        <translation type=Inserisci il PIN di Flickr></translation>
     </message>
     <message>
         <source>Unable to authenticate you with the entered PIN.</source>
-        <translation type="unfinished"></translation>
+        <translation type=Impossibile autenticarsi con il PIN inserito></translation>
     </message>
     <message>
         <source>Enter PIN again</source>
-        <translation type="unfinished"></translation>
+        <translation type=Inserisci di nuovo il PIN></translation>
     </message>
     <message>
         <source>Restart authentication</source>
-        <translation type="unfinished"></translation>
+        <translation type=Riprova ad autenticarti></translation>
     </message>
     <message>
         <source>Welcome to Fernweh!</source>
-        <translation type="unfinished"></translation>
+        <translation type=Benvenuto su Fernweh></translation>
     </message>
     <message>
         <source>Please login to Flickr to continue.</source>
-        <translation type="unfinished"></translation>
+        <translation type=Effettua l'accesso a Flickr></translation>
     </message>
     <message>
         <source>Log in to Flickr</source>
-        <translation type="unfinished"></translation>
+        <translation type=Accedi a Flickr></translation>
     </message>
     <message>
         <source>If you don&apos;t have a Flickr account yet, please sign up first.</source>
-        <translation type="unfinished"></translation>
+        <translation type=Se non hai un account di Flickr, registrati prima.></translation>
     </message>
     <message>
         <source>Sign up for Flickr</source>
-        <translation type="unfinished"></translation>
+        <translation type=Registrati a Flickr></translation>
     </message>
 </context>
 </TS>
